8039cbe506e48f2ae4c90d50c476e46a12030f8c
Virag Patel <19it114@charusat.edu.in>
2022-07-16 22:46:16 +0530

    New Files - #1

---
+++ youtube-transcript-summarizer-frontend/src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+import BackendAPI from './components/BackendApi';
+
+
+function App() {
+	return (
+		<div className="App">
+			<header className="App-header">
+				<h1>YouTube Transcript Summarizer</h1>
+				<pre><div class="line"></div></pre>
+				<BackendAPI />
+			</header>
+			{/* <footer className="footer">
+				Made by - 19IT121 Venis Prajapati And 19IT114 Virag Patel
+			</footer> */}
+		</div>
+	);
+}
+
+export default App;
